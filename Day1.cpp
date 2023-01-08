89e082aa5fd880b95c4d02a27f30765530f800d0
Shubham Kumar Singh <98223531+Shubhamkumar-op@users.noreply.github.com>
2023-01-08 13:43:50 +0530

    day_1

---
+++ Day1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+const ll M = 1e9+7;
+#define fr(i,n) for(ll i=0;i<n;i++)
+#define fr1(i,n) for(ll i=1;i<n;i++)
+#define cy cout<<"YES"<<"\n"
+#define fi fixed<<setprecision(0)
+#define cn cout<<"NO"<<"\n"
+#define all(x) (x).begin(),(x).end()
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	int t;
+	cin>>t;
+	while(t--){
+	    ll n,k;
+        ll s=0;
+        cin>>n>>k;
+        for(int i=1;i<=k;i++){
+          s+=i;
+        }
+        if(k==1){cy;}
+        else if(n>=s){cy;}
+        else {cn;}
+        
+        //  cout<<s<<endl;
+    }
+	    return 0;
+	}
