c4f4c9199fdf71aeebade9da25cf5c58c6e86ac5
Shubham Kumar Singh <98223531+Shubhamkumar-op@users.noreply.github.com>
2023-01-11 00:10:27 +0530

    Day 3

---
+++ Day3.cpp
@@ -0,0 +1,104 @@
+// // #include<bits/stdc++.h>
+// // #define ll long long int
+// // using namespace std;
+// // const ll M = 1e9+7;
+// // #define fr(i,n) for(ll i=0;i<n;i++)
+// // #define fr1(i,n) for(ll i=1;i<n;i++)
+// // #define cy cout<<"YES"<<"\n"
+// // #define fi fixed<<setprecision(0)
+// // #define cn cout<<"NO"<<"\n"
+// // #define all(x) (x).begin(),(x).end()
+
+// // int main() 
+// // {
+// //     ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+// // 	int t;
+// // 	cin>>t;
+// // 	while(t--){
+	  
+// //            ll n,k;
+// //            cin>>n>>k;
+           
+// //            ll a[n];
+// //            fr(i,n){
+// //             cin>>a[i];
+// //            }
+
+// //            fr(i,n){
+// //             if(a[i]<=k){cout<<"1";
+// //             k=k-a[i];
+// //             }
+// //             else{cout<<"0";}
+// //            }
+// // 	       cout<<endl;
+// // 	}
+// // 	    return 0;
+// // 	}
+
+
+// #include<bits/stdc++.h>
+// #define ll long long int
+// using namespace std;
+// const ll M = 1e9+7;
+// #define fr(i,n) for(ll i=0;i<n;i++)
+// #define fr1(i,n) for(ll i=1;i<n;i++)
+// #define cy cout<<"YES"<<"\n"
+// #define fi fixed<<setprecision(0)
+// #define cn cout<<"NO"<<"\n"
+// #define all(x) (x).begin(),(x).end()
+
+// int main() 
+// {
+//     ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+// 	int t;
+// 	cin>>t;
+// 	while(t--){
+// 	     ll a,b,c,d,e,f;
+// 	     cin>>a>>b>>c>>d>>e>>f;
+// 	     if((a==c || a==d) && (b==c || b==d)){cout<<"1"<<endl;}
+//          else if((a==e || a==f) && (b==e || b==f)){cout<<"2"<<endl;}
+//          else{cout<<"0"<<endl;}
+
+	       
+// 	}
+// 	    return 0;
+// 	}
+
+
+// #include<bits/stdc++.h>
+// #define ll long long int
+// using namespace std;
+// const ll M = 1e9+7;
+// #define fr(i,n) for(ll i=0;i<n;i++)
+// #define fr1(i,n) for(ll i=1;i<n;i++)
+// #define cy cout<<"YES"<<"\n"
+// #define fi fixed<<setprecision(0)
+// #define cn cout<<"NO"<<"\n"
+// #define all(x) (x).begin(),(x).end()
+
+// int main() 
+// {
+//     ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+// 	int t;
+// 	cin>>t;
+// 	while(t--){
+	     
+// 	      string s;
+//           cin>>s;
+//           ll a=0,b=0;
+//         //    bool f=1;
+// 	      for(ll i=0;i<s.size();i++){
+//                  if(s[i]=='A'|| s[i]=='D' || s[i]=='O'||s[i]=='P'||s[i]=='R'||s[i]=='Q'){
+//                  a+=1;
+//                  }
+//                  else if(s[i]=='B'){
+//                  b+=2;
+//                  }
+//             }
+// 	       cout<<a+b<<endl;
+	       
+// 	}
+// 	    return 0;
+// 	}
+
+
